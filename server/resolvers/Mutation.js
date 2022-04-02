c99e576c3074569cf534646848f79c0b7e7c29d7
haha246 <71302574+haha246@users.noreply.github.com>
2022-04-03 02:31:32 +0800

    tidy up

---
+++ server/resolvers/Mutation.js
@@ -0,0 +1,38 @@
+import Message from '../models/message'
+
+const Mutation = {
+	createMessage(parent, args, {pubsub}, info){
+		const user = JSON.parse(JSON.stringify(args.data))
+		
+		Message.create(user)
+		
+		pubsub.publish('message', {
+			message:{
+				mutation: 'CREATED',
+				data: user
+			}
+		})
+		
+		return user
+	},
+	
+	async deleteMessage(parent, args, {pubsub}, info){
+		const FindMes = await Message.findOne({"body": [args.body]})
+		
+		await Message.deleteMany({"body": [args.body]})
+		
+		pubsub.publish('message', {
+			message:{
+				mutation: 'DELETED',
+				data: {
+					name: FindMes.name,
+					body: FindMes.body   
+					  }
+			}
+		})
+		
+		return FindMes
+	}
+}
+
+export default Mutation
